4a87d5162bfb58183158a8e1b60d31f77036a47d
paulinatrillo <paulina.trillo@hotmail.com>
2023-06-06 14:57:12 -0300

    desafio

--- productmanager/productmanager.js
+++ productmanager/productmanager.js
@@ -11,21 +11,35 @@ class ProductManager {
             thumbnail,
             code,
             stock,
-            id: this.#getMaxId() + 1,
+            id,
         };
         this.products.push(product);
     }
 
-    #getMaxId() {
+    getProductById(id) {
+        const product = this.products.find((product) => product.id === id);
+        if (product) {
+            return product;
+        } else {
+            console.log("Producto no encontrado");
+            return null;
+        }
+    }
+
+    getMaxId() {
         let maxId = 0;
         this.products.map((product) => {
-        if (product.id > maxId) maxId = product.id;
-                
+            if (product.id > maxId) {
+                maxId = product.id;
+            }
         });
 
-    return maxId;
-}
-    getProducts(){
-        return this.products;
+        return maxId;
     }
 }
+
+
+
+
+
+
