913aeed61c46442969815cfde15a177b789cc34f
paulinatrillo <paulina.trillo@hotmail.com>
2023-06-05 21:26:53 -0300

    desafio

--- productmanager/productmanager.js
+++ productmanager/productmanager.js
@@ -1,5 +1,18 @@
-class ProductManager{
-    constructor () {
+class ProductManager {
+    constructor() {
         this.products = [];
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock, id) {
+        const product = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id,
+        };
+        this.products.push(product);
     }
 }
