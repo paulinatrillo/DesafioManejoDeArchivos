a1d8e6a9da38a320c390283d71eb64a0ac53a3cd
paulinatrillo <paulina.trillo@hotmail.com>
2023-06-12 17:48:54 -0300

    segundo desafio manejo de archivos

--- productmanager/productmanager.js
+++ productmanager/productmanager.js
@@ -1,53 +1,83 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-        this.products = [];
-    }
+  constructor() {
+    this.products = [];
+    this.path = './productos.json'; 
+    this.loadProducts();
+  }
 
-    addProduct(title, description, price, thumbnail, code, stock, id) {
-        const product = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-            id,
-        };
-        this.products.push(product);
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf-8');
+      this.products = JSON.parse(data);
+    } catch (error) {
+      console.log('Error al cargar los productos:', error);
     }
+  }
 
-    getProductById(id) {
-        const product = this.products.find((product) => product.id === id);
-        if (product) {
-            return product;
-        } else {
-            console.log("Producto no encontrado");
-            return null;
-        }
+  saveProducts() {
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2), 'utf-8');
+    } catch (error) {
+      console.log('Error al guardar los productos:', error);
     }
+  }
 
-    getMaxId() {
-        let maxId = 0;
-        this.products.map((product) => {
-            if (product.id > maxId) {
-                maxId = product.id;
-            }
-        });
+  addProduct(title, description, price, thumbnail, code, stock, id) {
+    const product = {
+      title,
+      description,
+      price,
+      thumbnail,
+      code,
+      stock,
+      id,
+    };
+    this.products.push(product);
+    this.saveProducts();
+  }
 
-        return maxId;
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.log('Producto no encontrado');
+      return null;
     }
+  }
+
+  getMaxId() {
+    let maxId = 0;
+    this.products.forEach((product) => {
+      if (product.id > maxId) {
+        maxId = product.id;
+      }
+    });
+
+    return maxId;
+  }
+
+  deleteProduct(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      this.saveProducts();
+      console.log('Producto eliminado correctamente');
+    } else {
+      console.log('Producto no encontrado');
+    }
+  }
 }
 
 const manager = new ProductManager();
 
-manager.addProduct('Cómoda americana','Medidas:1,40m de ancho,79cm de alto y 45cm de profundidad.',12500, '../src/assets/img/comoda.jpg', 'D43236', 4, 1);
-manager.addProduct('Mesas de luz americanas', 'Medidas:50cm de ancho,60cm de alto y 42de profundidad.',9000, '../src/assets/img/mesitasazul.jpeg', 'J54888', 13, 2);
-manager.addProduct('Cómoda degrade', 'Medidas:76cm de ancho,94cm de alto y 40cm de profundidad.',12000, '../src/assets/img/comodadegrade-min.jpeg', 'Z35653', 10, 3);
-manager.addProduct('Espejo francés blanco', 'Medidas:1,00m de ancho,70cm de alto.' ,4000, '../src/assets/img/espejo.jpg', 'B43978', 9, 4);
-manager.addProduct('Juego de comedor americano', 'Medidas:1,30m de ancho, 90cm de alto.' , 30000, '../src/assets/img/juegoamericano.jpg', 'O99000', 5, 5);
+manager.addProduct('Cómoda americana', 'Medidas:1,40m de ancho,79cm de alto y 45cm de profundidad.', 12500, '../src/assets/img/comoda.jpg', 'D43236', 4, 1);
+manager.addProduct('Mesas de luz americanas', 'Medidas:50cm de ancho,60cm de alto y 42de profundidad.', 9000, '../src/assets/img/mesitasazul.jpeg', 'J54888', 13, 2);
+manager.addProduct('Cómoda degrade', 'Medidas:76cm de ancho,94cm de alto y 40cm de profundidad.', 12000, '../src/assets/img/comodadegrade-min.jpeg', 'Z35653', 10, 3);
+manager.addProduct('Espejo francés blanco', 'Medidas:1,00m de ancho,70cm de alto.', 4000, '../src/assets/img/espejo.jpg', 'B43978', 9, 4);
+manager.addProduct('Juego de comedor americano', 'Medidas:1,30m de ancho, 90cm de alto.', 30000, '../src/assets/img/juegoamericano.jpg', 'O99000', 5, 5);
 
 console.log(manager.products);
-
-
-
 
