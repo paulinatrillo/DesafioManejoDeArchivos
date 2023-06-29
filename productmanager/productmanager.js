da1784fbc5dca621f9e5cfc16d32adc5725adae9
paulinatrillo <paulina.trillo@hotmail.com>
2023-06-29 16:23:52 -0300

    primera entrega proyecto final

--- productmanager/productmanager.js
+++ productmanager/productmanager.js
@@ -1,96 +1,119 @@
 import fs from 'fs';
 
 class ProductManager {
-    constructor(path) {
-        this.path = path;
-        this.products = [];
-        this.loadProducts();
-    }
+  constructor(path) {
+    this.path = path;
+    this.products = [];
+    this.loadProducts();
+  }
 
-    loadProducts() {
-        try {
-            const data = fs.readFileSync(this.path, 'utf-8');
-            this.products = JSON.parse(data);
-        } catch (error) {
-            console.log('Error al cargar los productos:', error.message);
-            this.products = [];
-        }
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'utf-8');
+      this.products = JSON.parse(data);
+    } catch (error) {
+      console.log('Error al cargar los productos:', error.message);
+      this.products = [];
     }
+  }
 
-    saveProducts() {
-        try {
-            const data = JSON.stringify(this.products, null, 2);
-            fs.writeFileSync(this.path, data);
-            console.log('Productos guardados exitosamente.');
-        } catch (error) {
-            console.log('Error al guardar los productos:', error.message);
-        }
+  saveProducts() {
+    try {
+      const data = JSON.stringify(this.products, null, 2);
+      fs.writeFileSync(this.path, data);
+      console.log('Productos guardados exitosamente.');
+    } catch (error) {
+      console.log('Error al guardar los productos:', error.message);
     }
+  }
 
-    addProduct(title, description, price, thumbnail, code, stock, id) {
-        const product = {
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-            id,
-        };
-        this.products.push(product);
-        this.saveProducts();
+  addProduct(product) {
+    const productId = this.generateProductId();
+    const newProduct = {
+      id: productId,
+      ...product,
+    };
+    this.products.push(newProduct);
+    this.saveProducts();
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    return product;
+  }
+
+  updateProduct(id, updatedProduct) {
+    const product = this.products.find((product) => product.id === id);
+    if (product) {
+      Object.assign(product, updatedProduct);
+      this.saveProducts();
     }
+  }
 
-    getProductById(id) {
-        const product = this.products.find((product) => product.id === id);
-        if (product) {
-            return product;
-        } else {
-            console.log("Producto no encontrado.");
-            return null;
-        }
+  deleteProduct(id) {
+    const index = this.products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      this.products.splice(index, 1);
+      this.saveProducts();
     }
+  }
 
-    getMaxId() {
-        let maxId = 0;
-        this.products.map((product) => {
-            if (product.id > maxId) {
-                maxId = product.id;
-            }
-        });
+  createCart(cart) {
+    const cartsData = this.readDataFromFile('carrito.json');
+    cartsData.carts.push(cart);
+    this.writeDataToFile('carrito.json', cartsData);
+  }
 
-        return maxId;
+  getCartById(id) {
+    const cartsData = this.readDataFromFile('carrito.json');
+    const cart = cartsData.carts.find((cart) => cart.id === id);
+    return cart;
+  }
+
+  addProductToCart(cartId, productId, quantity) {
+    const cartsData = this.readDataFromFile('carrito.json');
+    const cart = cartsData.carts.find((cart) => cart.id === cartId);
+    if (cart) {
+      const existingProduct = cart.products.find((product) => product.product === productId);
+      if (existingProduct) {
+        existingProduct.quantity += quantity;
+      } else {
+        cart.products.push({ product: productId, quantity });
+      }
+      this.writeDataToFile('carrito.json', cartsData);
     }
+  }
 
-    updateProduct(id, newData) {
-        const product = this.products.find((product) => product.id === id);
-        if (product) {
-            Object.assign(product, newData);
-            this.saveProducts();
-            console.log('Producto actualizado exitosamente.');
-        } else {
-            console.log('Producto no encontrado.');
-        }
-    }
+  readDataFromFile(filename) {
+    const data = fs.readFileSync(filename, 'utf-8');
+    return JSON.parse(data);
+  }
 
-    deleteProduct(id) {
-        const index = this.products.findIndex((product) => product.id === id);
-        if (index !== -1) {
-            this.products.splice(index, 1);
-            this.saveProducts();
-            console.log('Producto eliminado exitosamente.');
-        } else {
-            console.log('Producto no encontrado.');
-        }
+  writeDataToFile(filename, data) {
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFileSync(filename, jsonData);
+  }
+
+  generateProductId() {
+    let maxId = 0;
+    for (const product of this.products) {
+      if (product.id > maxId) {
+        maxId = product.id;
+      }
     }
+    return maxId + 1;
+  }
+
+  generateCartId() {
+    const cartsData = this.readDataFromFile('carrito.json');
+    let maxId = 0;
+    for (const cart of cartsData.carts) {
+      if (cart.id > maxId) {
+        maxId = cart.id;
+      }
+    }
+    return maxId + 1;
+  }
 }
-
-const manager = new ProductManager('./productos.json');
-
-manager.addProduct('Cómoda americana', 'Medidas:1,40m de ancho,79cm de alto y 45cm de profundidad.', 12500, '../src/assets/img/comoda.jpg', 'D43236', 4, 1);
-manager.addProduct('Mesas de luz americanas', 'Medidas:50cm de ancho,60cm de alto y 42de profundidad.', 9000, '../src/assets/img/mesitasazul.jpeg', 'J54888', 13, 2);
-manager.addProduct('Cómoda degrade', 'Medidas:76cm de ancho,94cm de alto y 40cm de profundidad.', 12000, '../src/assets/img/comodadegrade-min.jpeg', 'Z35653', 10, 3);
-manager.addProduct('Espejo francés blanco', 'Medidas:1,00m de ancho,70cm de alto.', 4000, '../src/assets/img/espejo.jpg', 'B43978', 9, 4);
-manager.addProduct('Juego de comedor americano', 'Medidas:1,30m de ancho, 90cm de alto.', 30000, '../src/assets/img/juegoamericano.jpg', 'O99000', 5, 5);
 
 export default ProductManager;
--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 
 const manager = new ProductManager('./productos.json');
 
-app.get('/products', async (req, res) => {
+
+app.get('/api/products', async (req, res) => {
   try {
     const { limit } = req.query;
     const products = await manager.getProducts(limit);
@@ -19,7 +20,7 @@ app.get('/products', async (req, res) => {
   }
 });
 
-app.get('/products/:pid', (req, res) => {
+app.get('/api/products/:pid', (req, res) => {
   const { pid } = req.params;
   const product = manager.getProductById(pid);
   if (product) {
@@ -27,6 +28,49 @@ app.get('/products/:pid', (req, res) => {
   } else {
     res.status(404).json({ message: 'Producto no encontrado' });
   }
+});
+
+app.post('/api/products', (req, res) => {
+  const { title, description, price, thumbnail, code, stock } = req.body;
+  const id = manager.getMaxId() + 1;
+  manager.addProduct(title, description, price, thumbnail, code, stock, id);
+  res.status(201).json({ message: 'Producto agregado exitosamente' });
+});
+
+app.put('/api/products/:pid', (req, res) => {
+  const { pid } = req.params;
+  const { title, description, price, thumbnail, code, stock } = req.body;
+  manager.updateProduct(pid, { title, description, price, thumbnail, code, stock });
+  res.status(200).json({ message: 'Producto actualizado exitosamente' });
+});
+
+app.delete('/api/products/:pid', (req, res) => {
+  const { pid } = req.params;
+  manager.deleteProduct(pid);
+  res.status(200).json({ message: 'Producto eliminado exitosamente' });
+});
+
+
+app.post('/api/carts', (req, res) => {
+  const cartId = manager.createCart();
+  res.status(201).json({ cartId });
+});
+
+app.get('/api/carts/:cid', (req, res) => {
+  const { cid } = req.params;
+  const cart = manager.getCartById(cid);
+  if (cart) {
+    res.status(200).json(cart);
+  } else {
+    res.status(404).json({ message: 'Carrito no encontrado' });
+  }
+});
+
+app.post('/api/carts/:cid/product/:pid', (req, res) => {
+  const { cid, pid } = req.params;
+  const { quantity } = req.body;
+  manager.addProductToCart(cid, pid, quantity);
+  res.status(200).json({ message: 'Producto agregado al carrito exitosamente' });
 });
 
 app.listen(port, () => {
