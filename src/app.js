f9a3bb68d93f495f2926fa788b8f24143a086617
paulinatrillo <paulina.trillo@hotmail.com>
2023-07-05 20:40:13 -0300

    motores y websocket

--- src/app.js
+++ src/app.js
@@ -1,14 +1,36 @@
 import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
 import routesProducts from './routes/routesProducts.js';
+import viewsRouter from './routes/viewsRouter.js';
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = 8080;
+
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+
+  socket.on('productCreated', () => {
+    socket.emit('productAdded');
+  });
+
+  socket.on('productDeleted', () => {
+    socket.emit('productRemoved');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', routesProducts);
+app.use('/', viewsRouter);
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
